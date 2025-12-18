05964bfd727b8306932307b08602fddafcdb7bf2
Tamir Dingjan <tamir.dingjan@gmail.com>
2025-12-18 09:41:03 +0200

    make report script rename bad stereo patches

--- patches/report_bad_stereo.sh
+++ patches/report_bad_stereo.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-for patch in ./patch_*
+for patch in patch_*
 do
 	if [ -f $patch/md0.gro ]; then
 		if grep -q False $patch/stereo.check; then
 			echo $patch
+			mv $patch bad_stereo_$patch
 		fi
 	fi
 done
